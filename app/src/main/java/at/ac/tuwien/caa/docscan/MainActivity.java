e9fc78c61124bf6284cb77c296227e4a1eddfe13
hollaus <holl@caa.tuwien.ac.at>
2016-06-06 17:07:32 +0200

    first commit simple SurfaceView for camera preview

---
+++ app/src/main/java/at/ac/tuwien/caa/docscan/MainActivity.java
@@ -0,0 +1,89 @@
+package at.ac.tuwien.caa.docscan;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.hardware.Camera;
+import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.FrameLayout;
+
+import org.opencv.android.OpenCVLoader;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Camera mCamera;
+    private CameraPreview mPreview;
+
+
+    static {
+
+        boolean init = OpenCVLoader.initDebug();
+
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+//        requestPermission();
+        initCamera();
+
+    }
+
+
+    /** A safe way to get an instance of the Camera object. */
+    public static Camera getCameraInstance(){
+        Camera c = null;
+        try {
+            c = Camera.open(); // attempt to get a Camera instance
+        }
+        catch (Exception e){
+            // Camera is not available (in use or does not exist)
+        }
+        return c; // returns null if camera is unavailable
+    }
+
+    private void requestPermission() {
+
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                Manifest.permission.CAMERA)) {
+
+        }
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+//        initCamera();
+        switch (requestCode) {
+            case 10: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    initCamera();
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+
+                }
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request
+        }
+    }
+
+    private void initCamera() {
+
+        // Create an instance of Camera
+        mCamera = Camera.open(0);
+
+        // Create our Preview view and set it as the content of our activity.
+        mPreview = new CameraPreview(this, mCamera);
+        FrameLayout preview = (FrameLayout) findViewById(R.id.camera_preview);
+        preview.addView(mPreview);
+
+    }
+}
