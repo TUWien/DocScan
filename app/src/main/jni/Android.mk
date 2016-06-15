0d81b3987184ed2a5f07b1a5e29b59b26c3bd38e
hollaus <holl@caa.tuwien.ac.at>
2016-06-15 15:58:06 +0200

    Code cleanup

--- app/src/main/jni/Android.mk
+++ app/src/main/jni/Android.mk
@@ -9,7 +9,8 @@ OPENCV_INSTALL_MODULES:=on
 OPENCV_LIB_TYPE:=SHARED
 include ${OPENCVROOT}/sdk/native/jni/OpenCV.mk
 
-LOCAL_SRC_FILES := opencv-jni.cpp
+//LOCAL_SRC_FILES := opencv-jni.cpp
+LOCAL_SRC_FILES := NativeWrapper.cpp opencv-jni.cpp
 LOCAL_LDLIBS += -llog
 LOCAL_MODULE := opencv-jni
 LOCAL_CFLAGS += -std=c++11
