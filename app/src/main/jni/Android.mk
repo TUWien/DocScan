f74a7316b84895eba8d6fa31d80cf8e05f4055d2
hollaus <holl@caa.tuwien.ac.at>
2016-07-01 09:39:43 +0200

    removed absolute path from main gradle file removed absolute path from Android.mk

--- app/src/main/jni/Android.mk
+++ app/src/main/jni/Android.mk
@@ -3,16 +3,22 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 #opencv
-OPENCVROOT:= C:\VSProjects\OpenCV3-android
+# include file with user defined OpenCV SDK path:
+include $(LOCAL_PATH)/local/Android.mk
+OPENCVROOT:= $(MY_OPENCVROOT)
+
 OPENCV_CAMERA_MODULES:=off
 OPENCV_INSTALL_MODULES:=on
 OPENCV_LIB_TYPE:=SHARED
 include ${OPENCVROOT}/sdk/native/jni/OpenCV.mk
 
-LOCAL_SRC_FILES := FocusMeasure.cpp DkMath.cpp PageSegmentationUtils.cpp PageSegmentation.cpp DocScanInterface.cpp
+# Compile all CPP files, by not using LOCAL_SRC_FILES
+#LOCAL_SRC_FILES := DocScanInterface.cpp FocusMeasure.cpp
 LOCAL_LDLIBS += -llog
 LOCAL_MODULE := docscan-native
 LOCAL_CFLAGS += -std=c++11
 LOCAL_LDFLAGS += -ljnigraphics
+
+LOCAL_CPPFLAGS += -DJNI_BUILD
 
 include $(BUILD_SHARED_LIBRARY)
