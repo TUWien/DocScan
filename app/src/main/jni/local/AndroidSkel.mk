bf8b1cab045b467878ef675fca9e97ac0d57ad13
hollaus <holl@caa.tuwien.ac.at>
2016-07-01 10:13:00 +0200

    added local/AndroidSkel.mk for setting up local OpenCV path

---
+++ app/src/main/jni/local/AndroidSkel.mk
@@ -0,0 +1,3 @@
+# Set here the path to your OpenCV Android SDK (contains the folders: apk, sample, sdk
+# Uncomment the following line and set your local path here:
+# MY_OPENCVROOT:= C:/cvl/dmrz/code/opencv_sdk/OpenCV-android-sdk 
