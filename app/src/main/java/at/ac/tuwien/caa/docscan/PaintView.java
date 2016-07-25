1f2783e437ba6340fa6e68c4e8ccd1013539f7e4
hollaus <holl@caa.tuwien.ac.at>
2016-07-25 16:52:32 +0200

    added file saving methods

--- app/src/main/java/at/ac/tuwien/caa/docscan/PaintView.java
+++ app/src/main/java/at/ac/tuwien/caa/docscan/PaintView.java
@@ -8,12 +8,13 @@ import android.graphics.Path;
 import android.graphics.PixelFormat;
 import android.graphics.PointF;
 import android.graphics.PorterDuff;
+import android.graphics.Rect;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
 import java.util.ArrayList;
+import java.util.Locale;
 
 import at.ac.tuwien.caa.docscan.cv.DkPolyRect;
 import at.ac.tuwien.caa.docscan.cv.Patch;
@@ -25,13 +26,15 @@ import at.ac.tuwien.caa.docscan.cv.Patch;
 
 public class PaintView extends SurfaceView implements SurfaceHolder.Callback {
 
+
     private DrawerThread mDrawerThread;
     private TaskTimer.TimerCallbacks mTimerCallbacks;
 
-    private Patch[] mFocusPatches;
-    private DkPolyRect[] mPolyRects;
+    private boolean mDrawFocusText;
+    private SurfaceHolder mHolder;
 
     private CVResult mCVResult;
+
 
     private static final String TAG = "PaintView";
 
@@ -44,12 +47,22 @@ public class PaintView extends SurfaceView implements SurfaceHolder.Callback {
         holder.addCallback(this);
         // This is necessary to enable semi-transparent DrawView
         holder.setFormat(PixelFormat.TRANSLUCENT);
+        mHolder = holder;
+
         mTimerCallbacks = (TaskTimer.TimerCallbacks) context;
+
+        mDrawerThread = new DrawerThread(holder);
+
+        mDrawFocusText = false;
 
     }
 
 
+    public void drawFocusText(boolean drawText) {
 
+        mDrawFocusText = drawText;
+
+    }
 
 
     public void setCVResult(CVResult cvResult) {
@@ -62,21 +75,18 @@ public class PaintView extends SurfaceView implements SurfaceHolder.Callback {
     public void surfaceCreated(SurfaceHolder holder)
     {
 
-        mDrawerThread = new DrawerThread(holder);
-
+        mHolder = holder;
         mDrawerThread.setRunning(true);
 
-
-        // TODO: check why the thread is already started - if the app is restarted. The thread should be dead!
         if (mDrawerThread.getState() == Thread.State.NEW)
             mDrawerThread.start();
-
 
     }
 
     @Override
     public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
 
+        mHolder = holder;
 
         if (mDrawerThread != null)
             mDrawerThread.setSurfaceSize(width, height);
@@ -101,8 +111,29 @@ public class PaintView extends SurfaceView implements SurfaceHolder.Callback {
 
     public void resume() {
 
-        if (mDrawerThread != null)
-            mDrawerThread.setRunning(true);
+
+
+        if (mDrawerThread != null) {
+            // It is necessary to call notify here, because otherwise there will be a deadlock in the
+            // run method, which waits for the mCVResult object. The deadlock will arise after the
+            // app is resumed (for example after an orientation change).
+            synchronized (mCVResult) {
+                mCVResult.notify();
+            }
+
+            if (mDrawerThread.getState() == Thread.State.TERMINATED) {
+
+                mDrawerThread = new DrawerThread(mHolder);
+                mDrawerThread.setRunning(true);
+                mDrawerThread.start();
+
+            }
+            else
+                mDrawerThread.setRunning(true);
+
+
+
+        }
 
 
     }
@@ -121,17 +152,6 @@ public class PaintView extends SurfaceView implements SurfaceHolder.Callback {
 
     }
 
-    public void setFocusPatches(Patch[] focusPatches) {
-
-        mFocusPatches = focusPatches;
-
-    }
-
-    public void setDkPolyRects(DkPolyRect[] dkPolyRects) {
-
-        mPolyRects = dkPolyRects;
-
-    }
 
     class DrawerThread extends Thread {
 
@@ -141,14 +161,21 @@ public class PaintView extends SurfaceView implements SurfaceHolder.Callback {
         private Paint mTextPaint;
         private Paint mSegmentationPaint;
         private Path mSegmentationPath;
+
+        private static final float RECT_HALF_SIZE = 30;
+        private static final String TEXT_FORMAT = "%.2f";
+
         private final int GOOD_TEXT_COLOR = getResources().getColor(R.color.hud_bad_text_color);
         private final int BAD_TEXT_COLOR = getResources().getColor(R.color.hud_good_text_color);
         private final int PAGE_RECT_COLOR = getResources().getColor(R.color.hud_page_rect_color);
+        private final int FOCUS_SHARP_RECT_COLOR = getResources().getColor(R.color.hud_focus_sharp_rect_color);
+        private final int FOCUS_UNSHARP_RECT_COLOR = getResources().getColor(R.color.hud_focus_unssharp_rect_color);
 
         private int mCanvasWidth, mCanvasHeight;
 
 
-        private Paint mRectPaint;
+        private Paint mFocusSharpRectPaint;
+        private Paint mFocusUnsharpRectPaint;
 
         // Used for debug output:
 
@@ -161,10 +188,11 @@ public class PaintView extends SurfaceView implements SurfaceHolder.Callback {
 
             // Used to print out measured focus:
             mTextPaint = new Paint();
-
+            // Get device independent screen units:
             // Taken from here: http://stackoverflow.com/questions/3061930/how-to-set-unit-for-paint-settextsize
             int scaledFontSize = getResources().getDimensionPixelSize(R.dimen.draw_view_focus_font_size);
             mTextPaint.setTextSize(scaledFontSize);
+
 
             // Used to paint the page segmentation boundaries:
             mSegmentationPaint = new Paint();
@@ -174,37 +202,28 @@ public class PaintView extends SurfaceView implements SurfaceHolder.Callback {
             mSegmentationPaint.setStrokeWidth(7);
             mSegmentationPath = new Path();
 
-            // Used for debugging rectangle
-            mRectPaint = new Paint();
+
+            mFocusSharpRectPaint = new Paint();
+            mFocusSharpRectPaint.setStrokeWidth(2);
+            mFocusSharpRectPaint.setStyle(Paint.Style.STROKE);
+            mFocusSharpRectPaint.setColor(FOCUS_SHARP_RECT_COLOR);
+
+            mFocusUnsharpRectPaint = new Paint();
+            mFocusUnsharpRectPaint.setStrokeWidth(2);
+            mFocusUnsharpRectPaint.setStyle(Paint.Style.STROKE);
+            mFocusUnsharpRectPaint.setColor(FOCUS_UNSHARP_RECT_COLOR);
 
         }
-
-
-
-//        private void setFocusPatches(Patch[] focusPatches) {
-//
-//            mFocusPatches = focusPatches;
-//
-//        }
-//
-//        private void setPolyRects(DkPolyRect[] polyRects) {
-//
-//            mPolyRects = polyRects;
-//
-//        }
 
         @Override
         public void run() {
 
-
-// TODO: Clean up this mess! This causes some seconds after orientation change:
-
             while (mIsRunning) {
 
-//                    synchronized (mSurfaceHolder) {
-//
                 Canvas canvas = null;
 //
+                // This wait is used to assure that the drawing function is just called after an update
+                // from the native package:
                 synchronized (mCVResult) {
 
                     try {
@@ -212,7 +231,6 @@ public class PaintView extends SurfaceView implements SurfaceHolder.Callback {
                     } catch (InterruptedException e) {
 
                     }
-////                    }
 
                     try {
                         canvas = mSurfaceHolder.lockCanvas();
@@ -228,71 +246,9 @@ public class PaintView extends SurfaceView implements SurfaceHolder.Callback {
 
                 }
 
-//                }
-
-
-//            if (mCameraView == null)
-//                return;
-
-//            if (mSurfaceHolder == null)
-//                return;
-//
-//            synchronized (mSurfaceHolder) {
-//
-//                while (mIsRunning) {
-//
-//                    Canvas canvas = null;
-//
-//                    try {
-//
-//                        canvas = mSurfaceHolder.lockCanvas(null);
-//
-////                        synchronized (mCameraView) {
-////
-////                            try {
-////
-////                                mCameraView.wait();
-////
-////
-////                            } catch (InterruptedException e) {
-////
-////                            }
-////                        }
-//
-//
-//                    if (MainActivity.isDebugViewEnabled())
-//                        mTimerCallbacks.onTimerStarted(TaskTimer.DRAW_VIEW_ID);
-//
-//                    draw(canvas);
-//
-//                    if (MainActivity.isDebugViewEnabled())
-//                        mTimerCallbacks.onTimerStopped(TaskTimer.DRAW_VIEW_ID);
-//
-////                        }
-//
-//                    } finally {
-//                        // do this in a finally so that if an exception is thrown
-//                        // during the above, we don't leave the Surface in an
-//                        // inconsistent state
-//                        if (canvas != null) {
-//                            mSurfaceHolder.unlockCanvasAndPost(canvas);
-//                        }
-//                    }
-//                }
-//            }
             }
         }
 
-
-//        /* Callback invoked when the surface dimensions change. */
-//        public void setSurfaceSize(int width, int height) {
-//            // synchronized to make sure these all change atomically
-//            synchronized (mSurfaceHolder) {
-//                mCanvasWidth = width;
-//                mCanvasHeight = height;
-//
-//            }
-//        }
 
         /* Callback invoked when the surface dimensions change. */
         public void setSurfaceSize(int width, int height) {
@@ -316,8 +272,6 @@ public class PaintView extends SurfaceView implements SurfaceHolder.Callback {
 
         private void draw(Canvas canvas) {
 
-            Log.d(TAG, "draw");
-
             if (canvas == null) {
                 return;
             }
@@ -325,102 +279,104 @@ public class PaintView extends SurfaceView implements SurfaceHolder.Callback {
 //            Clear the screen from previous drawings:
             canvas.drawColor(Color.BLUE, PorterDuff.Mode.CLEAR);
 
-//            Debugging rectangle:
-//            mRectPaint.setARGB(200,0,100,0);
-////            canvas.drawRect(mCanvasWidth - 200, mCanvasHeight - 200, mCanvasWidth + 200, mCanvasHeight + 200, mRectPaint);
-//
-////            canvas.drawRect(0, 0, mCanvasWidth, mCanvasHeight, mRectPaint);
-////            mRectPaint.setARGB(255,100,100,0);
-//            canvas.drawRect(0, 0, mCanvasWidth - 100, mCanvasHeight - 100, mRectPaint);
-
-
             if (mCVResult != null) {
-                if (mCVResult.getPatches() != null) {
 
-//                    Patch patch;
 
-                    for (Patch patch : mCVResult.getPatches()) {
+                // Focus measure:
+                if (mCVResult.getPatches() != null)
+                    drawFocusMeasure(canvas);
 
-                        String fValue = String.format("%.2f", patch.getFM());
+                // Page segmentation:
+                if (mCVResult.getDKPolyRects() != null)
+                    drawPageSegmentation(canvas);
 
-                        if (patch.getIsForeGround()) {
-                            if (patch.getIsSharp())
-                                mTextPaint.setColor(GOOD_TEXT_COLOR);
-                            else
-                                mTextPaint.setColor(BAD_TEXT_COLOR);
+            }
+        }
 
-                            canvas.drawText(fValue, patch.getDrawViewPX(), patch.getDrawViewPY() + 50, mTextPaint);
-                        }
+        private void drawPageSegmentation(Canvas canvas) {
 
-                    }
+            for (DkPolyRect dkPolyRect : mCVResult.getDKPolyRects()) {
+
+                mSegmentationPath.reset();
+
+                ArrayList<PointF> screenPoints = dkPolyRect.getScreenPoints();
+
+                boolean isStartSet = false;
+
+                for (PointF point : screenPoints) {
+
+                    if (!isStartSet) {
+                        mSegmentationPath.moveTo(point.x, point.y);
+                        isStartSet = true;
+                    } else
+                        mSegmentationPath.lineTo(point.x, point.y);
 
                 }
 
-                if (mCVResult.getDKPolyRects() != null) {
+                mSegmentationPath.close();
+                canvas.drawPath(mSegmentationPath, mSegmentationPaint);
 
-                    for (DkPolyRect dkPolyRect : mCVResult.getDKPolyRects()) {
 
-                        mSegmentationPath.reset();
+            }
 
-                        ArrayList<PointF> screenPoints = dkPolyRect.getScreenPoints();
+        }
 
-                        boolean isStartSet = false;
+        private void drawFocusMeasure(Canvas canvas) {
 
-                        for (PointF point : screenPoints) {
+            Rect textRect = new Rect();
+            float textWidth = -1;
+//            float textHeight = -1;
 
-                            if (!isStartSet) {
-                                mSegmentationPath.moveTo(point.x, point.y);
-                                isStartSet = true;
-                            } else
-                                mSegmentationPath.lineTo(point.x, point.y);
+            for (Patch patch : mCVResult.getPatches()) {
 
+                String fValue = String.format(Locale.ENGLISH, TEXT_FORMAT, patch.getFM());
+
+                if (patch.getIsForeGround()) {
+
+                    if (patch.getIsSharp()) {
+
+                        if (mDrawFocusText)
+                            mTextPaint.setColor(GOOD_TEXT_COLOR);
+
+                        canvas.drawRect(patch.getDrawViewPX() - RECT_HALF_SIZE, patch.getDrawViewPY() - RECT_HALF_SIZE,
+                                patch.getDrawViewPX() + RECT_HALF_SIZE, patch.getDrawViewPY() + RECT_HALF_SIZE, mFocusSharpRectPaint);
+
+                    }
+                    else {
+
+                        if (mDrawFocusText)
+                            mTextPaint.setColor(BAD_TEXT_COLOR);
+
+                        canvas.drawRect(patch.getDrawViewPX() - RECT_HALF_SIZE, patch.getDrawViewPY() - RECT_HALF_SIZE,
+                                patch.getDrawViewPX() + RECT_HALF_SIZE, patch.getDrawViewPY() + RECT_HALF_SIZE, mFocusUnsharpRectPaint);
+                    }
+
+
+                    if (mDrawFocusText) {
+
+                        // Get the size of the text in screen units so that we can center the text:
+                        // (Do this just for the first iteration.)
+                        if (textWidth == -1) {
+                            mTextPaint.getTextBounds(fValue, 0, fValue.length(), textRect);
+                            textWidth = (float) textRect.width() / 2;
+//                            textHeight = (float) textRect.height() / 2;
                         }
 
-                        mSegmentationPath.close();
-                        canvas.drawPath(mSegmentationPath, mSegmentationPaint);
+                        canvas.drawText(fValue, patch.getDrawViewPX() - textWidth,
+                                patch.getDrawViewPY() + RECT_HALF_SIZE, mTextPaint);
 
                     }
 
                 }
 
             }
-//
-//            if (mPolyRects != null) {
-//
-//                for (DkPolyRect polyRect : mPolyRects) {
-//
-//                    // TODO: find out why this is partially NULL:
-//                    if (polyRect == null)
-//                        continue;
-//
-//                    mSegmentationPath.reset();
-//
-//                    ArrayList<PointF> screenPoints = polyRect.getScreenPoints();
-//
-//                    boolean isStartSet = false;
-//
-//                    for (PointF point : screenPoints) {
-//
-//                        if (!isStartSet) {
-//                            mSegmentationPath.moveTo(point.x, point.y);
-//                            isStartSet = true;
-//                        }
-//                        else
-//                            mSegmentationPath.lineTo(point.x, point.y);
-//
-//                    }
-//
-//                    mSegmentationPath.close();
-//
-//                    canvas.drawPath(mSegmentationPath, mSegmentationPaint);
-//
-//                }
-//
-//            }
-//
+
+
         }
 
     }
+
+
 
 
  }
