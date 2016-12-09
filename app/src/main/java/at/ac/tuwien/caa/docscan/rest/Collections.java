7a70e57e4c482dff8468bc973461cca707cb8c6d
hollaus <holl@caa.tuwien.ac.at>
2016-12-09 20:51:33 +0100

    rest api request handling

---
+++ app/src/main/java/at/ac/tuwien/caa/docscan/rest/Collections.java
@@ -0,0 +1,9 @@
+package at.ac.tuwien.caa.docscan.rest;
+
+/**
+ * Created by fabian on 05.12.2016.
+ */
+public class Collections {
+
+//    public Collections(String )
+}
--- app/src/main/java/at/ac/tuwien/caa/docscan/ui/CollectionsActivity.java
+++ app/src/main/java/at/ac/tuwien/caa/docscan/ui/CollectionsActivity.java
@@ -3,12 +3,15 @@ package at.ac.tuwien.caa.docscan.ui;
 import android.os.Bundle;
 
 import at.ac.tuwien.caa.docscan.R;
-import at.ac.tuwien.caa.docscan.rest.RestQuest;
+import at.ac.tuwien.caa.docscan.rest.CollectionsRequest;
+import at.ac.tuwien.caa.docscan.rest.LoginRequest;
+import at.ac.tuwien.caa.docscan.rest.RequestHandler;
+import at.ac.tuwien.caa.docscan.rest.User;
 
 /**
  * Created by fabian on 30.11.2016.
  */
-public class CollectionsActivity extends BaseActivity {
+public class CollectionsActivity extends BaseActivity implements LoginRequest.LoginCallback, CollectionsRequest.CollectionsCallback {
 
 
     @Override
@@ -17,12 +20,26 @@ public class CollectionsActivity extends BaseActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_collections);
 
-        RestQuest.createLoginRequest(this);
+        RequestHandler.createRequest(this, RequestHandler.REQUEST_LOGIN);
 
     }
 
     @Override
     protected NavigationDrawer.NavigationItemEnum getSelfNavDrawerItem() {
+
         return NavigationDrawer.NavigationItemEnum.COLLECTIONS;
+
+    }
+
+    @Override
+    public void onLogin(User user) {
+
+        RequestHandler.createRequest(this, RequestHandler.REQUEST_COLLECTIONS);
+
+    }
+
+    @Override
+    public void onCollections(User user) {
+
     }
 }
