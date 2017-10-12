a948450b07de0f33364c979cfed8f2f506ed741f
hollaus <holl@caa.tuwien.ac.at>
2017-10-12 16:19:38 +0200

    fixed selection bug in SyncAdapter

--- app/src/main/java/at/ac/tuwien/caa/docscan/ui/syncui/SyncAdapter.java
+++ app/src/main/java/at/ac/tuwien/caa/docscan/ui/syncui/SyncAdapter.java
@@ -2,6 +2,7 @@ package at.ac.tuwien.caa.docscan.ui.syncui;
 
 import android.content.Context;
 import android.graphics.Typeface;
+import android.util.SparseBooleanArray;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -18,9 +19,15 @@ import at.ac.tuwien.caa.docscan.R;
 
 public class SyncAdapter extends BaseDocumentAdapter {
 
+    private SparseBooleanArray mSelections;
+
 
     public SyncAdapter(Context context) {
+
         super(context);
+
+        mSelections = new SparseBooleanArray();
+
     }
 
 
@@ -32,12 +39,29 @@ public class SyncAdapter extends BaseDocumentAdapter {
             LayoutInflater infalInflater = (LayoutInflater) mContext
                     .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
             convertView = infalInflater.inflate(R.layout.checkbox_list_group, null);
+
+            // We have to store the state of the checkbox manually, because the views are recycled.
+            // Got this solution from
+            // https://stackoverflow.com/questions/35285635/when-click-on-expandable-listview-then-checked-checkbox-is-automatically-uncheck
+            CheckBox checkBoxListHeader = (CheckBox) convertView.findViewById(R.id.checkboxListHeader);
+            checkBoxListHeader.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    int position = (int) v.getTag();
+                    mSelections.put(position, !mSelections.get(position, false));
+                    ((CheckBox)v).setChecked(mSelections.get(position, false));
+                }
+            });
+
         }
 
-        CheckBox checkBoxListHeader = (CheckBox) convertView
-                .findViewById(R.id.checkboxListHeader);
+        CheckBox checkBoxListHeader = (CheckBox) convertView.findViewById(R.id.checkboxListHeader);
         checkBoxListHeader.setTypeface(null, Typeface.BOLD);
         checkBoxListHeader.setText(headerTitle);
+        // reassigning checkbox to its ticked state
+        checkBoxListHeader.setTag(groupPosition);
+        checkBoxListHeader.setChecked(mSelections.get(groupPosition, false));
+
 
         return convertView;
 
