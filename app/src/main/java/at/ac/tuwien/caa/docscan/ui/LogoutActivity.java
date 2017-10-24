6ed2bb761b80c08a9992e0958fa7565e7a94462e
hollaus <holl@caa.tuwien.ac.at>
2017-10-24 16:45:18 +0200

    added logout functionality

--- app/src/main/java/at/ac/tuwien/caa/docscan/ui/LogoutActivity.java
+++ app/src/main/java/at/ac/tuwien/caa/docscan/ui/LogoutActivity.java
@@ -1,17 +1,19 @@
 package at.ac.tuwien.caa.docscan.ui;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 import at.ac.tuwien.caa.docscan.R;
+import at.ac.tuwien.caa.docscan.rest.LogoutRequest;
 import at.ac.tuwien.caa.docscan.rest.User;
 
 /**
  * Created by fabian on 24.08.2017.
  */
 
-public class LogoutActivity extends BaseNavigationActivity {
+public class LogoutActivity extends BaseNavigationActivity implements LogoutRequest.LogoutCallback {
 
 
     @Override
@@ -20,11 +22,14 @@ public class LogoutActivity extends BaseNavigationActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_logout);
 
+        final Context context = this;
+
         Button confirmButton = (Button) findViewById(R.id.logout_confirm_button);
         confirmButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 // TODO: think about logout here:
+                new LogoutRequest(context);
                 User.getInstance().setLoggedIn(false);
                 // Close the activity:
                 finish();
@@ -42,9 +47,13 @@ public class LogoutActivity extends BaseNavigationActivity {
 
     }
 
-//    @Override
-//    protected NavigationDrawer.NavigationItemEnum getSelfNavDrawerItem() {
-//        return NavigationDrawer.NavigationItemEnum.ACCOUNT_LOGOUT;
-//    }
+    @Override
+    protected NavigationDrawer.NavigationItemEnum getSelfNavDrawerItem() {
+        return NavigationDrawer.NavigationItemEnum.ACCOUNT_LOGOUT;
+    }
 
+    @Override
+    public void onLogout() {
+        User.getInstance().setLoggedIn(false);
+    }
 }
