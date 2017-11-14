870fc950060471aa4c114064010da6c9bd07302b
hollaus <holl@caa.tuwien.ac.at>
2017-11-14 13:30:29 +0100

    fixed SSLHandshakeException on android 4.X devices

--- app/src/main/java/at/ac/tuwien/caa/docscan/rest/RequestHandler.java
+++ app/src/main/java/at/ac/tuwien/caa/docscan/rest/RequestHandler.java
@@ -5,20 +5,30 @@ package at.ac.tuwien.caa.docscan.rest;
  */
 
 import android.content.Context;
+import android.os.Build;
+import android.util.Log;
 
 import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.HttpStack;
+import com.android.volley.toolbox.HurlStack;
 import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
+import com.google.android.gms.common.GooglePlayServicesRepairableException;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.security.ProviderInstaller;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
 
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -41,7 +51,43 @@ public class RequestHandler {
 
     }
 
+    /**
+     * Creates a new request queue. If API is between JELLY_BEAN and KIT_KAT a HttpStack with
+     * TLSSocketFactory is generated. This fixes the SSLHandshakeException on Android 4 devices.
+     * Taken from: https://stackoverflow.com/questions/31269425/how-do-i-tell-the-tls-version-in-android-volley
+     * @param context
+     * @return
+     */
+    public static RequestQueue createRequestQueue(Context context) {
+
+        RequestQueue requestQueue;
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN
+                && Build.VERSION.SDK_INT <= Build.VERSION_CODES.KITKAT) {
+            HttpStack stack = null;
+            try {
+                stack = new HurlStack(null, new TLSSocketFactory());
+            } catch (KeyManagementException e) {
+                e.printStackTrace();
+                Log.d("Your Wrapper Class", "Could not create new stack for TLS v1.2");
+                stack = new HurlStack();
+            } catch (NoSuchAlgorithmException e) {
+                e.printStackTrace();
+                Log.d("Your Wrapper Class", "Could not create new stack for TLS v1.2");
+                stack = new HurlStack();
+            }
+            requestQueue = Volley.newRequestQueue(context, stack);
+        } else {
+            requestQueue = Volley.newRequestQueue(context);
+        }
+
+        return requestQueue;
+    }
+
     public static void processLoginRequest(final LoginRequest request) {
+
+        checkProviderInstaller(request);
+
 
         StringRequest stringRequest = new StringRequest(Request.Method.POST, request.getURL(),
                 new Response.Listener<String>() {
@@ -68,11 +114,34 @@ public class RequestHandler {
             }
         };
 
-        //Adding the string request to the queue
-        RequestQueue requestQueue = Volley.newRequestQueue(request.getContext());
+        RequestQueue requestQueue = createRequestQueue(request.getContext());
         requestQueue.add(stringRequest);
 
 
+    }
+
+    /**
+     * Checks if the ProviderInstaller is up-to-date. This is necessary to fix the SSL
+     * SSLHandshakeException on Android 4 devices.
+     * as it is stated here: https://stackoverflow.com/questions/31269425/how-do-i-tell-the-tls-version-in-android-volley
+     * Could not reproduce that this is really necessary, since TLSSocketFactory already did the trick.
+     * (Google Play Services installed on testing devices were not too old.)
+     * @param request
+     */
+    private static void checkProviderInstaller(LoginRequest request) {
+        try {
+            ProviderInstaller.installIfNeeded(request.getContext());
+        } catch (GooglePlayServicesRepairableException e) {
+
+            // Indicates that Google Play services is out of date, disabled, etc.
+            // Prompt the user to install/update/enable Google Play services.
+            GooglePlayServicesUtil.showErrorNotification(
+                    e.getConnectionStatusCode(), request.getContext());
+
+        } catch (GooglePlayServicesNotAvailableException e) {
+            // Indicates a non-recoverable error; the ProviderInstaller is not able
+            // to install an up-to-date Provider.
+        }
     }
 
     public static void processRequest(final RestRequest.XMLRequest request) {
@@ -102,7 +171,8 @@ public class RequestHandler {
         };
 
         //Adding the string request to the queue
-        RequestQueue requestQueue = Volley.newRequestQueue(request.getContext());
+//        RequestQueue requestQueue = Volley.newRequestQueue(request.getContext());
+        RequestQueue requestQueue = createRequestQueue(request.getContext());
         requestQueue.add(stringRequest);
     }
 
@@ -116,7 +186,8 @@ public class RequestHandler {
 
 
         //Adding the string request to the queue
-        RequestQueue requestQueue = Volley.newRequestQueue(request.getContext());
+//        RequestQueue requestQueue = Volley.newRequestQueue(request.getContext());
+        RequestQueue requestQueue = createRequestQueue(request.getContext());
         requestQueue.add(r);
 
     }
@@ -124,7 +195,8 @@ public class RequestHandler {
     public static void processMultiPartRequest(Context context,  MultipartRequest multipartRequest) {
 
         //Adding the string request to the queue
-        RequestQueue requestQueue = Volley.newRequestQueue(context);
+//        RequestQueue requestQueue = Volley.newRequestQueue(context);
+        RequestQueue requestQueue = createRequestQueue(context);
         requestQueue.add(multipartRequest);
 
     }
