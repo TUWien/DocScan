ccc575a46f8d5aef13cb8e12ae520e05fc143e38
hollaus <hollaus@cvl.tuwien.ac.at>
2018-04-05 17:08:02 +0200

    added share button to PageSlideActivitiy, without functionality

--- app/src/main/java/at/ac/tuwien/caa/docscan/ui/gallery/PageSlideActivity.java
+++ app/src/main/java/at/ac/tuwien/caa/docscan/ui/gallery/PageSlideActivity.java
@@ -17,29 +17,56 @@ package at.ac.tuwien.caa.docscan.ui.gallery;
  * limitations under the License.
  */
 
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.content.FileProvider;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+import com.davemorrissey.labs.subscaleview.ImageSource;
 
 import java.io.File;
 import java.io.FileFilter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 
+import at.ac.tuwien.caa.docscan.BuildConfig;
 import at.ac.tuwien.caa.docscan.R;
+import at.ac.tuwien.caa.docscan.crop.CropInfo;
 import at.ac.tuwien.caa.docscan.gallery.ImageViewerFragment;
+import at.ac.tuwien.caa.docscan.gallery.TouchImageView;
 import at.ac.tuwien.caa.docscan.logic.Document;
 import at.ac.tuwien.caa.docscan.logic.Helper;
 import at.ac.tuwien.caa.docscan.logic.Page;
+import at.ac.tuwien.caa.docscan.ui.CropViewActivity;
 
-public class PageSlideActivity extends AppCompatActivity {
+import static at.ac.tuwien.caa.docscan.crop.CropInfo.CROP_INFO_NAME;
+
+public class PageSlideActivity extends AppCompatActivity implements TouchImageView.SingleClickListener {
 
     private ViewPager mPager;
-    private PagerAdapter mPagerAdapter;
+    private PageSlideAdapter mPagerAdapter;
+    private Page mPage;
+    private Toolbar mToolbar;
+    private Document mDocument;
+    private LinearLayout mButtonsLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,48 +74,207 @@ public class PageSlideActivity extends AppCompatActivity {
         setContentView(R.layout.activity_page_slide);
 
         // Instantiate a ViewPager and a PagerAdapter.
-        mPager = (ViewPager) findViewById(R.id.slide_viewpager);
+        initPager();
 
-//        mPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
-//            @Override
-//            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
-//
-//            }
-//
-//            @Override
-//            public void onPageSelected(int position) {
-////                mPager.setCurrentItem(mPager.getCurrentItem());
-//            }
-//
-//            @Override
-//            public void onPageScrollStateChanged(int state) {
-//
-//            }
-//        });
-
-//        Create a dummy document based on the intent: TODO: use a real document here
         String fileName = getIntent().getStringExtra("DOCUMENT_FILE_NAME");
         if (fileName == null)
             return;
 
-        Document document = getDummyDocument(fileName);
+        mDocument = getDummyDocument(fileName);
 
-        mPagerAdapter = new PageSlideAdapter(getSupportFragmentManager(), document);
+        mPagerAdapter = new PageSlideAdapter(getSupportFragmentManager(), mDocument);
         mPager.setAdapter(mPagerAdapter);
-        mPager.setOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener() {
-            @Override
-            public void onPageSelected(int position) {
-                // When changing pages, reset the action bar actions since they are dependent
-                // on which page is currently active. An alternative approach is to have each
-                // fragment expose actions itself (rather than the activity exposing actions),
-                // but for simplicity, the activity provides the actions in this sample.
-                invalidateOptionsMenu();
-            }
-        });
+
+        initToolbar();
+        initButtons();
+
+        mButtonsLayout = findViewById(R.id.page_view_buttons_layout);
+
 
         int position = getIntent().getIntExtra("PAGE_POSITION", -1);
         if (position != -1)
             mPager.setCurrentItem(position);
+
+    }
+
+    private void initPager() {
+        mPager = findViewById(R.id.slide_viewpager);
+
+
+        mPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+
+                mPage = mDocument.getPages().get(position);
+
+//                mPager.setCurrentItem(mPager.getCurrentItem());
+//          TODO: uncomment later:
+//                String title = mPage.getFile().getName();
+//                getSupportActionBar().setTitle(title);
+
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+    }
+
+    private void initToolbar() {
+
+        mToolbar = findViewById(R.id.image_viewer_toolbar);
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        mToolbar.setPadding(0, getStatusBarHeight(), 0, 0);
+
+        // Close the fragment if the user hits the back button in the toolbar:
+        mToolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+
+    }
+
+    // A method to find height of the status bar
+    public int getStatusBarHeight() {
+
+        int result = 0;
+        int resourceId = getResources().getIdentifier("status_bar_height", "dimen", "android");
+        if (resourceId > 0) {
+            result = getResources().getDimensionPixelSize(resourceId);
+        }
+        return result;
+
+    }
+
+
+    private void initButtons() {
+
+        initCropButton();
+        initDeleteButton();
+        initRotateButton();
+        initShareButton();
+
+    }
+
+    private void initRotateButton() {
+
+        ImageView rotateImageView = findViewById(R.id.page_view_buttons_layout_rotate_button);
+        rotateImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mPage != null) {
+                    try {
+//                        Rotate the image 90 degrees and set the image again (I did not find another way to force an update of the imageview)
+                        if (Helper.rotateExif(mPage.getFile().getAbsoluteFile())) {
+                            mPagerAdapter.getCurrentFragment().refreshImageView();
+//                              Tell the gallery viewer that the file has rotated:
+                            GalleryActivity.fileRotated();
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+
+                }
+            }
+        });
+
+    }
+
+    private void initShareButton() {
+
+//        TODO: not working
+
+        ImageView shareView = findViewById(R.id.page_view_buttons_layout_share_button);
+        shareView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mPage != null) {
+
+//                    Uri uri = FileProvider.getUriForFile(getApplicationContext(), getApplicationContext().getPackageName(), mPage.getFile());
+                    Uri uri = FileProvider.getUriForFile(getApplicationContext(), "at.ac.tuwien.caa.fileprovider", mPage.getFile());
+
+                    Intent shareIntent = new Intent();
+                    shareIntent.setAction(Intent.ACTION_SEND);
+                    shareIntent.putExtra(Intent.EXTRA_STREAM, mPage.getFile().getAbsolutePath());
+                    shareIntent.setType("image/jpeg");
+                    startActivity(Intent.createChooser(shareIntent, "asdf"));
+
+                }
+            }
+        });
+
+    }
+
+
+    private void initCropButton() {
+
+        ImageView cropImageView = findViewById(R.id.page_view_buttons_layout_crop_button);
+        cropImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mPage != null) {
+                    Intent intent = new Intent(getApplicationContext(), CropViewActivity.class);
+                    CropInfo cropInfo = new CropInfo(mPage.getFile().getPath());
+                    intent.putExtra(CROP_INFO_NAME, cropInfo);
+                    startActivity(intent);
+                }
+            }
+        });
+
+    }
+
+    private void initDeleteButton() {
+
+        ImageView deleteImageView = findViewById(R.id.page_view_buttons_layout_delete_button);
+        deleteImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mPage != null) {
+                    showDeleteConfirmationDialog();
+                }
+            }
+        });
+
+    }
+
+    private void showDeleteConfirmationDialog() {
+
+
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+
+        // set dialog message
+        alertDialogBuilder
+                .setTitle(R.string.page_slide_fragment_confirm_delete_text)
+                .setPositiveButton(R.string.page_slide_fragment_confirm_delete_confirm_title, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i)  {
+                        mPage.getFile().delete();
+//                            Tell the gallery viewer that the file was deleted:
+                        GalleryActivity.fileDeleted();
+                        finish();
+
+                    }
+                })
+                .setNegativeButton(R.string.page_slide_fragment_confirm_delete_cancel_text, null)
+                .setCancelable(true);
+//                .setMessage(deleteText);
+
+        // create alert dialog
+        AlertDialog alertDialog = alertDialogBuilder.create();
+
+        // show it
+        alertDialog.show();
+
     }
 
 
@@ -146,10 +332,24 @@ public class PageSlideActivity extends AppCompatActivity {
 
     }
 
+    @Override
+    public void onSingleClick() {
+
+        if (mToolbar.getVisibility() == View.VISIBLE) {
+            mToolbar.setVisibility(View.INVISIBLE);
+            mButtonsLayout.setVisibility(View.INVISIBLE);
+        }
+        else {
+            mToolbar.setVisibility(View.VISIBLE);
+            mButtonsLayout.setVisibility(View.VISIBLE);
+        }
+
+    }
+
     private class PageSlideAdapter extends FragmentStatePagerAdapter {
 
         private Document mDocument;
-
+        private ImageViewerFragment mCurrentFragment;
 
         public PageSlideAdapter(FragmentManager fm, Document document) {
 
@@ -173,6 +373,22 @@ public class PageSlideActivity extends AppCompatActivity {
             return mDocument.getPages().size();
 
         }
+
+        @Override
+        public void setPrimaryItem(ViewGroup container, int position, Object object) {
+            if (getCurrentFragment() != object) {
+                mCurrentFragment = ((ImageViewerFragment) object);
+            }
+            super.setPrimaryItem(container, position, object);
+        }
+
+
+
+        public ImageViewerFragment getCurrentFragment() {
+            return mCurrentFragment;
+        }
+
+
     }
 
 
