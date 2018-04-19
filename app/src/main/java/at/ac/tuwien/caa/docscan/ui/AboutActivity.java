a3de24249ccf5ff7d217388f934cc6300c7bd87d
hollaus <hollaus@cvl.tuwien.ac.at>
2018-04-19 16:17:01 +0200

    added adaptive icon added build time to AboutActivity

--- app/src/main/java/at/ac/tuwien/caa/docscan/ui/AboutActivity.java
+++ app/src/main/java/at/ac/tuwien/caa/docscan/ui/AboutActivity.java
@@ -4,6 +4,10 @@ import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import at.ac.tuwien.caa.docscan.BuildConfig;
 import at.ac.tuwien.caa.docscan.R;
 
 /**
@@ -17,12 +21,21 @@ public class AboutActivity extends BaseNavigationActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
 
-        // Show the app version number:
+        // Show the app version number and build time:
         try {
             String versionName = getResources().getString(R.string.about_version_prefix_text);
             versionName +=  " " + getPackageManager().getPackageInfo(getPackageName(), 0).versionName;
             TextView versionTextView = findViewById(R.id.about_version_textview);
             versionTextView.setText(versionName);
+
+            String buildTime = getResources().getString(R.string.about_buildtime_prefix_text);
+            Date buildDate = new Date(BuildConfig.TIMESTAMP);
+            TextView buildDateTextView = findViewById(R.id.about_buildtime_textview);
+            SimpleDateFormat sdf = new SimpleDateFormat();
+            String date = sdf.format(buildDate);
+            buildTime += " " + date;
+            buildDateTextView.setText(buildTime);
+
         } catch (PackageManager.NameNotFoundException e) {
             e.printStackTrace();
         }
