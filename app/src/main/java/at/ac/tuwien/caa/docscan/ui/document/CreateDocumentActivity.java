ec3f2f2ee7ab8cfac5362f953bf6e0c0b248a0d9
hollaus <holl@caa.tuwien.ac.at>
2018-07-05 14:45:33 +0200

    fixes bugs: java.lang.NullPointerException: at at.ac.tuwien.caa.docscan.logic.Helper.getDocuments (Helper.java:332)

--- app/src/main/java/at/ac/tuwien/caa/docscan/ui/document/CreateDocumentActivity.java
+++ app/src/main/java/at/ac/tuwien/caa/docscan/ui/document/CreateDocumentActivity.java
@@ -203,7 +203,7 @@ public class CreateDocumentActivity extends BaseNoNavigationActivity {
                 != PackageManager.PERMISSION_GRANTED)
             // ask for permission:
             ActivityCompat.requestPermissions(this, new String[]{
-                    Manifest.permission.READ_EXTERNAL_STORAGE}, PERMISSION_WRITE_EXTERNAL_STORAGE);
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE}, PERMISSION_WRITE_EXTERNAL_STORAGE);
         else
             createDir();
 
