4dc388197dcfb9b7f9d2baa93afa36edb77ed15f
hollaus <holl@caa.tuwien.ac.at>
2018-10-03 11:44:45 +0200

    switched to cmake for ndk setup

--- app/src/main/jni/Application.mk
+++
@@ -1,4 +0,0 @@
-APP_STL := gnustl_static
-APP_CPPFLAGS := -frtti -fexceptions
-APP_PLATFORM := android-16
-APP_ABI := armeabi-v7a
--- app/src/main/jni/local/AndroidSkel.mk
+++
@@ -1,3 +0,0 @@
-# Set here the path to your OpenCV Android SDK (contains the folders: apk, sample, sdk
-# Uncomment the following line and set your local path here:
-# MY_OPENCVROOT:= C:/somepath/OpenCV-android-sdk 
