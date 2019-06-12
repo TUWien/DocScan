e1e9f602c4f331fb9418072a288600877911048e
hollaus <holl@caa.tuwien.ac.at>
2019-06-12 14:56:50 +0200

    added minimal example for junit test

---
+++ app/src/test/java/at/ac/tuwien/caa/docscan/MapperTest.kt
@@ -0,0 +1,39 @@
+package at.ac.tuwien.caa.docscan
+
+import android.util.Log
+import at.ac.tuwien.caa.docscan.camera.cv.thread.crop.Mapper
+import org.junit.Assert.assertTrue
+import org.junit.Test
+import org.opencv.android.OpenCVLoader
+
+// NOTE: this is not working, because we need the OpenCV executables for this, it just shows how to
+// do unit testing without mockup.
+
+class MapperTest {
+
+
+
+//    companion object {
+//        val CLASS_NAME = "MapperTest"
+//        init {
+//            if (!OpenCVLoader.initDebug()) {
+//                Log.d(CLASS_NAME, "Error while initializing OpenCV.")
+//            } else {
+//
+//                System.loadLibrary("opencv_java3")
+//                System.loadLibrary("docscan-native")
+//
+//                Log.d(CLASS_NAME, "OpenCV initialized")
+//            }
+//        }
+//    }
+
+    @Test
+    fun dummyTest() {
+
+
+
+//        Mapper.replaceWithMappedImage("asdf", null)
+
+    }
+}
